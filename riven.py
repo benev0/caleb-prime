5c8d327fbefe1ed8ee39c2655cacff0f03a91e6e
benev0 <benjaminevanownby@gmail.com>
2024-03-23 16:02:03 -0400

    got list of endo sources

---
+++ riven.py
@@ -0,0 +1,29 @@
+
+class RivenAuction:
+    def __init__(self, id, price, mastery_level, mod_rank, rerolls):
+        self.id = id
+        self.price = price
+        self.mastery_level = mastery_level
+        self.mod_rank = mod_rank
+        self.rerolls = rerolls
+        self.endoValue = self.__calEndoValue()
+        self.endoPerPlat = self.__calEndoPerPlat()
+
+    def __calEndoValue(self) -> int:
+        return 100 * (self.mastery_level - 8) + int(22.5 * 2**self.mod_rank) + 200 * self.rerolls - 7
+
+    def __calEndoPerPlat(self) -> int:
+        return self.__calEndoValue() / self.price
+
+    def __str__(self) -> str:
+        return f"id:    {self.id}\nprice: {self.price}\nendo:  {self.endoValue}\nepp:   {self.endoPerPlat}\n"
+
+    # def __cmp__(self):
+    #     return super.cmp(self.endoPerPlat, self.price)
+
+    def __lt__(self, other):
+        return self.endoPerPlat < other.endoPerPlat or (self.endoPerPlat == other.endoPerPlat and self.endoValue < other.endoValue)
+
+    def __eq__(self, other):
+        return self.endoPerPlat == other.endoPerPlat and self.endoValue == other.endoValue
+
